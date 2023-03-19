b1745fddbf780f8ccb6cad48298f1f1bd6be7549
Łukasz Ostrowski <LukaszOst@gmail.com>
2023-03-19 19:56:38 +0100

    Menu optimization

--- DrinkItUp/DrinkItUp.ConsoleUI/DiffMenu.cs
+++ DrinkItUp/DrinkItUp.ConsoleUI/DiffMenu.cs
@@ -9,7 +9,7 @@ namespace DrinkItUp.ConsoleUI
         {
             bool isSelected = false;
             int option = 1;
-            string color = "✅ \u001b[32m";
+            string color = "\u001b[32m";
             int selectedoption = 0;
             Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Wybierz opcje z Menu"));
             Console.WriteLine();
@@ -19,11 +19,17 @@ namespace DrinkItUp.ConsoleUI
             {
 
                 Console.SetCursorPosition(left, top);
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 1 ? color : "   ")} Łatwy\u001b[0m");
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 2 ? color : "   ")} Średni\u001b[0m");
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 3 ? color : "   ")} Trudny\u001b[0m");
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 4 ? color : "   ")} Wszystkie\u001b[0m");
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 5 ? color : "   ")} Wstecz\u001b[0m");
+                MainMenu.CursorPosition();
                 Console.WriteLine($"{(option == 6 ? color : "   ")} Wróć do Menu Głównego\u001b[0m");
 
 
