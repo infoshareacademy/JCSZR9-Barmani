2350ba1477bea5604f0bec42a3ae0eb1a5522899
kUcLis <ulis89@gmail.com>
2023-04-16 20:12:40 +0200

    Ongoing changes in templates to fit the style of the project Drink it Up, Added Razor Runtime Compilation :)

--- DrinkItUpWebApp/DrinkItUpWebApp/Program.cs
+++ DrinkItUpWebApp/DrinkItUpWebApp/Program.cs
@@ -7,7 +7,8 @@ namespace DrinkItUpWebApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
