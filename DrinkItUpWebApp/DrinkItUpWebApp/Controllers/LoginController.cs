5507ef8e3f55df03e7570f28a2037c782f819f52
Jarek <jaroslaw.pela@wp.pl>
2023-05-06 20:01:31 +0200

    Add Login - model

--- DrinkItUpWebApp/DrinkItUpWebApp/Controllers/LoginController.cs
+++ DrinkItUpWebApp/DrinkItUpWebApp/Controllers/LoginController.cs
@@ -38,46 +38,46 @@ namespace DrinkItUpWebApp.Controllers
             }
         }
 
-        // GET: LoginController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //// GET: LoginController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: LoginController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: LoginController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: LoginController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: LoginController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: LoginController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: LoginController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
---
+++ DrinkItUpWebApp/DrinkItUpWebApp/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿namespace DrinkItUpWebApp.Models
+{
+    public class Login
+    {
+
+        public int Id { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string Password { get; set; }
+
+        public string EMail { get; set; }
+
+
+
+    }
+}
