3ee6cdde163f7fffec58a1fdf6fce8145b4e3e55
Michal Krezlewicz <michal.krezlewicz@gmail.com>
2023-05-11 16:15:42 +0200

    Added DrinkController with methods: - DrinkSearch - DrinkShow - DrinkMixer Added view for DrinkShow

---
+++ DrinkItUpWebApp/DrinkItUpWebApp/Controllers/DrinkController .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkItUpWebApp.Controllers
+{
+    public class DrinkController : Controller
+    {
+        public IActionResult DrinkSearch()
+        {
+            return View();
+        }
+
+        public IActionResult DrinkShow()
+        {
+            return View();
+        }
+
+        public IActionResult DrinkMixer()
+        {
+            return View();
+        }
+    }
+}
