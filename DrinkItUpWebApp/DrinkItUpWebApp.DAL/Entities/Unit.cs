66a0e7b09d23e1f344a81c9a279e2553873c02b2
kUcLis <ulis89@gmail.com>
2023-05-14 11:46:03 +0200

    Added New DAL Project to solution,  Added Entity Unit

---
+++ DrinkItUpWebApp/DrinkItUpWebApp.DAL/Entities/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkItUpWebApp.DAL.Entities
+{
+    public class Unit
+    {
+        public int UnitId { get; set; }
+
+        public string UnitName { get; set; }
+    }
+}
