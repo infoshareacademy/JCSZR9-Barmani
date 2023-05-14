799398dcd912efa60846c7233d97091c80c704bd
Michal Krezlewicz <michal.krezlewicz@gmail.com>
2023-05-14 12:38:46 +0200

    DrinkIngredient Class added

---
+++ DrinkItUpWebApp/DrinkItUpWebApp.DAL/Entities/DrinkIngredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkItUpWebApp.DAL.Entities
+{
+    public class DrinkIngredient
+    {
+        public decimal Quantity { get; set; }
+
+        public int DrinkId { get; set; }
+
+        public Drink Drink { get; set; }
+
+        public int IngredientId { get; set; }
+
+        public Ingredient Ingredient { get; set; }
+    }
+}
