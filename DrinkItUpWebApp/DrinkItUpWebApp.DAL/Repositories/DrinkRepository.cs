c5d8d827eef892b813a76795082240777f8eb3d6
Michal Krezlewicz <michal.krezlewicz@gmail.com>
2023-05-21 14:08:34 +0200

    initial commit

---
+++ DrinkItUpWebApp/DrinkItUpWebApp.DAL/Repositories/DrinkRepository.cs
@@ -0,0 +1,38 @@
+﻿using DrinkItUpWebApp.DAL.Entities;
+using DrinkItUpWebApp.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkItUpWebApp.DAL.Repositories
+{
+    public class DrinkRepository : CRUDRepository<Drink>, IDrinkRepository
+    {
+        private DrinkContext _context;
+
+        public DrinkRepository(DrinkContext drinkContext) : base(drinkContext)
+        {
+            _context = drinkContext;
+        }
+        public IQueryable<Drink> SearchByNameQueryable(string name)
+        {
+            var drinks = _context.Drinks
+                .Include(d => d.MainAlcohol);
+
+
+               
+                //.Include(d => d.MainAlcohol)
+                //.Include(d => d.Difficulty)
+                //.Include(d => d.DrinkIngredients)
+                //.ThenInclude(d => d.Ingredient)
+                //.Where(d => d.Name == name)
+                //.AsQueryable();
+
+            //return drinks;
+        }
+    }
+}
