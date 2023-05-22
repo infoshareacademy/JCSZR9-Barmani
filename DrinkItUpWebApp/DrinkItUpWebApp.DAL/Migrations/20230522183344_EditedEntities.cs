b60a519a197498128343d603b45a38a4780268ca
kUcLis <ulis89@gmail.com>
2023-05-22 20:34:42 +0200

    New Migration

---
+++ DrinkItUpWebApp/DrinkItUpWebApp.DAL/Migrations/20230522183344_EditedEntities.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrinkItUpWebApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditedEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Drinks_DifficultyId",
+                table: "Drinks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drinks_MainAlcoholId",
+                table: "Drinks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drinks_DifficultyId",
+                table: "Drinks",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drinks_MainAlcoholId",
+                table: "Drinks",
+                column: "MainAlcoholId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Drinks_DifficultyId",
+                table: "Drinks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drinks_MainAlcoholId",
+                table: "Drinks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drinks_DifficultyId",
+                table: "Drinks",
+                column: "DifficultyId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drinks_MainAlcoholId",
+                table: "Drinks",
+                column: "MainAlcoholId",
+                unique: true);
+        }
+    }
+}
