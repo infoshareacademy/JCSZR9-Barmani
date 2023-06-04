8b06d325b5bbd7b3e89f35999393a5ba9029acb0
Michal Krezlewicz <michal.krezlewicz@gmail.com>
2023-06-04 13:38:27 +0200

    usings in GetDrinkDetails.cs updated

--- DrinkItUpWebApp/DrinkItUpBusinessLogic/GetDrinkDetails.cs
+++ DrinkItUpWebApp/DrinkItUpBusinessLogic/GetDrinkDetails.cs
@@ -2,12 +2,7 @@
 using DrinkItUpBusinessLogic.DTOs;
 using DrinkItUpBusinessLogic.Interfaces;
 using DrinkItUpWebApp.DAL.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DrinkItUpBusinessLogic
 {
