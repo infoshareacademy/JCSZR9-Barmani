6f37e9b9a638cc33d160614fb7ed40251f169751
kUcLis <ulis89@gmail.com>
2023-06-29 13:00:23 +0200

    Made Class in tests for WebFactory with method to receive client

--- DrinkItUpWebApp/DrinkItUpTests/SeperatedUnitRepositoryTest.cs
+++ DrinkItUpWebApp/DrinkItUpTests/SeperatedUnitRepositoryTest.cs
@@ -11,36 +11,7 @@ namespace DrinkItUpTests
 {
     public class SeperatedUnitRepositoryTest
     {
-        private readonly HttpClient _client;
-
-        public SeperatedUnitRepositoryTest()
-        {
-            var projectDir = Directory.GetCurrentDirectory();
-            var configPath = Path.Combine(projectDir, "appsettings.json");
-
-            var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureServices(services =>
-                {
-                    var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType ==
-                            typeof(DbContextOptions<DrinkContext>));
-
-                    services.Remove(descriptor);
-
-                    services.AddDbContext<DrinkContext>(options =>
-                    {
-                        options.UseInMemoryDatabase("InMemoryDatabase");
-                    });
-
-                });
-                builder.ConfigureAppConfiguration((context, config) =>
-                {
-                    config.AddJsonFile(configPath);
-                });
-            });
-            _client = factory.CreateClient();
-        }
+        private readonly HttpClient _client = new DrinktItUpWebFactory().GetClient();
 
 
         [Fact]
