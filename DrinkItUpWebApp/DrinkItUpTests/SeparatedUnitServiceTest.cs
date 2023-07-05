756a38049907d1864d17474233ac85c1bf1f8f91
kUcLis <ulis89@gmail.com>
2023-07-05 17:19:43 +0200

    Made Container class to handle Dependency Injection

--- DrinkItUpWebApp/DrinkItUpTests/SeparatedUnitServiceTest.cs
+++ DrinkItUpWebApp/DrinkItUpTests/SeparatedUnitServiceTest.cs
@@ -6,6 +6,8 @@ using DrinkItUpWebApp.MapperProfile;
 using DrinkItUpBusinessLogic;
 using DrinkItUpBusinessLogic.DTOs;
 using FluentAssertions;
+using DrinkItUpBusinessLogic.Interfaces;
+using System.ComponentModel.Design;
 
 namespace DrinkItUpTests
 {
@@ -14,30 +16,13 @@ namespace DrinkItUpTests
     public class SeparatedUnitServiceTest
     {
         
-        private static DbContextOptionsBuilder<DrinkContext> OptionsBuilder()
-        {
-            return new DbContextOptionsBuilder<DrinkContext>().UseInMemoryDatabase("InMemoryDatabase");
-        }
-
-
-        private static DrinkContext db = new DrinkContext(SeparatedUnitServiceTest.OptionsBuilder().Options);
-
-
-        private static UnitRepository _unitRepository = new UnitRepository(db);
-        private static DrinkIngredientRepository drinkIngredientRepository = new DrinkIngredientRepository(db);
-
-        private static IngredientRepository _ingredientRepository = new IngredientRepository(db, drinkIngredientRepository);
-
-        private static MapperConfiguration configuration = new MapperConfiguration(configurationBuilder => configurationBuilder.AddProfile<MapperProfile>());
-
-        private IMapper mapper = configuration.CreateMapper();
 
         [Fact]
         public async Task UnitService_AddUnit_ReturnAddedUnitName()
         {
             //Assign
-
-            var unitService = new UnitService(_unitRepository,mapper, _ingredientRepository);
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
             var unitDto = new UnitDto { Name = "jednostka" };
 
             //Act
@@ -48,14 +33,17 @@ namespace DrinkItUpTests
 
             testUnit.Name.Should().Be(unitDto.Name);
 
+            //Clearing Context
 
+            serviceContainer.EndOfTest();
         }
 
         [Fact]
         public async Task UnitService_GetById_ReturnUnitByID()
         {
             //Assign
-            var unitService = new UnitService(_unitRepository, mapper, _ingredientRepository);
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
             var unitDto = new UnitDto { Name = "jednostka" };
             var testUnit = await unitService.AddUnit(unitDto);
 
@@ -68,13 +56,15 @@ namespace DrinkItUpTests
             result.Name.Should().Be(unitDto.Name);
             result.UnitId.Should().Be(1);
 
+            serviceContainer.EndOfTest();
         }
 
         [Fact]
         public async Task UnitService_GetById_ReturnEmptyUnit()
         {
             //Assign
-            var unitService = new UnitService(_unitRepository, mapper, _ingredientRepository);
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
 
             //Act
             var result = await unitService.GetById(1);
@@ -84,7 +74,92 @@ namespace DrinkItUpTests
             result.Should().NotBeNull();
             result.Name.Should().BeNull();
 
+            serviceContainer.EndOfTest();
         }
 
+        [Fact]
+        public async Task UnitService_GetAll_ReturnAllUnits()
+        {
+            //Assign
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
+            var unitDto1 = new UnitDto { Name = "Kopa" };
+            var unitDto2 = new UnitDto { Name = "Mendel" };
+            await unitService.AddUnit(unitDto1);
+            await unitService.AddUnit(unitDto2);
+
+            //Act
+            var result = await unitService.GetAll();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.Count().Should().Be(2);
+            //do wyboru jedna z dwoch powyzszych metod budowania result
+            serviceContainer.EndOfTest();
+        }
+
+
+
+        [Fact]
+        public async Task UnitService_IsUnitUsed_ReturnsFalse()
+        {
+            //Assign
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
+            var unitDto = new UnitDto { Name = "Kopa" };
+            await unitService.AddUnit(unitDto);
+
+            //Act
+            var result = await unitService.IsUnitUsed(1);
+
+            //Assert
+            result.Should().BeFalse();
+
+            serviceContainer.EndOfTest();
+
+        }
+
+        [Fact]
+        public async Task UnitService_IsUnitUnique_ReturnsFalse()
+        {
+            //Assing
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
+            var unitDto = new UnitDto { Name = "Mendel" };
+            await unitService.AddUnit(unitDto);
+
+            //Act
+            var result = await unitService.IsUnitUnique(unitDto.Name);
+
+            //Assert
+            result.Should().BeFalse();
+
+            serviceContainer.EndOfTest();
+        }
+
+        [Fact]
+        public async Task UnitService_Update_ReturnsUpdatedDto()
+        //test nie przechodzi!!
+        {
+            //Assign
+            var serviceContainer = new Container();
+            var unitService = serviceContainer.GetUnitService();
+            var unitDto = new UnitDto { Name = "jednostka" };
+            await unitService.AddUnit(unitDto);
+            unitDto = await unitService.GetById(1);
+            var unitDtoUpdated = new UnitDto { UnitId = 1, Name = "Tuzin" };
+
+            serviceContainer.DetachModel();
+            //Act
+
+            await unitService.Update(unitDtoUpdated);
+            var unitFromDatabase = await unitService.GetById(1);
+
+            //Assert
+            unitDtoUpdated.Name.Should().Be(unitFromDatabase.Name);
+
+            serviceContainer.EndOfTest();
+        }
     }
 }
