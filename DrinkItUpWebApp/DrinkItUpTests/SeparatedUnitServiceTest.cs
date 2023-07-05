f5c9cfa3aa1ca5b92669148d6175f550a225a109
kUcLis <ulis89@gmail.com>
2023-07-05 20:24:15 +0200

    Refactor

--- DrinkItUpWebApp/DrinkItUpTests/SeparatedUnitServiceTest.cs
+++ DrinkItUpWebApp/DrinkItUpTests/SeparatedUnitServiceTest.cs
@@ -72,57 +72,9 @@ namespace DrinkItUpTests
             result.Name.Should().BeNull();
         }
 
-        [Fact]
-        public async Task UnitService_GetAll_ReturnAllUnits()
-        {
-            //Assign
-            var unitService = new UnitService(_unitRepository, mapper, _ingredientRepository);
-            var unitDto1 = new UnitDto { Name = "Kopa" };
-            var unitDto2 = new UnitDto { Name = "Mendel" };
-            await unitService.AddUnit(unitDto1);
-            await unitService.AddUnit(unitDto2);
 
-            //Act
-            var result = await unitService.GetAll();
+       
 
-            //Assert
-            result.Should().NotBeNull();
-            result.Should().HaveCount(2);
-            result.Count().Should().Be(2);
-            //do wyboru jedna z dwoch powyzszych metod budowania result
-        }
-
-
-
-        [Fact]
-        public async Task UnitService_IsUnitUsed_ReturnsFalse()
-        {
-            //Assign
-            var unitService = new UnitService(_unitRepository, mapper, _ingredientRepository);
-            var unitDto = new UnitDto { Name = "Kopa" };
-            await unitService.AddUnit(unitDto);
-
-            //Act
-            var result = await unitService.IsUnitUsed(1);
-
-            //Assert
-            result.Should().BeFalse();
-
-        }
-
-        [Fact]
-        public async Task UnitService_IsUnitUnique_ReturnsFalse()
-        {
-            //Assing
-            var unitService = new UnitService(_unitRepository, mapper, _ingredientRepository);
-            var unitDto = new UnitDto { Name = "Mendel" };
-            await unitService.AddUnit(unitDto);
-
-            //Act
-            var result = await unitService.IsUnitUnique(unitDto.Name);
-
-            serviceContainer.EndOfTest();
-        }
 
         [Fact]
         public async Task UnitService_GetAll_ReturnAllUnits()
