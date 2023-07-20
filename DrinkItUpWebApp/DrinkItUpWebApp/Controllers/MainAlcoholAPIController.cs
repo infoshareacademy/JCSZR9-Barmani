e9ad63379f4597ed947476e17a81022d21376d8b
Michal Krezlewicz <michal.krezlewicz@gmail.com>
2023-07-20 21:42:56 +0200

    MainAlcoholAPIController updated

--- DrinkItUpWebApp/DrinkItUpWebApp/Controllers/MainAlcoholAPIController.cs
+++ DrinkItUpWebApp/DrinkItUpWebApp/Controllers/MainAlcoholAPIController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using DrinkItUpBusinessLogic.DTOs;
 using DrinkItUpBusinessLogic.Interfaces;
+using DrinkItUpWebApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,12 +20,67 @@ namespace DrinkItUpWebApp.Controllers
             _mainAlcoholService = mainAlcoholService;
         }
 
+
         [HttpGet]
         [Route("GetAll")]
         public async Task<IActionResult> GetAll()
         {
             var mainAlcoholsDtos = await _mainAlcoholService.GetAll();
             return Ok(mainAlcoholsDtos);
+        }
+
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var mainAlcoholDtos = await _mainAlcoholService.GetById(id);
+            return Ok(mainAlcoholDtos);
+        }
+
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add([FromBody] MainAlcoholDto mainAlcoholDto)
+        {
+            if (!await _mainAlcoholService.IsMainAlcoholUnique(mainAlcoholDto.Name))
+                return BadRequest("Name is already used");
+
+            var addedMainAlcoholDto = await _mainAlcoholService.AddMainAlcohol(mainAlcoholDto);
+            return Ok(addedMainAlcoholDto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("update")]
+
+        public async Task<ActionResult> Update([FromBody] MainAlcoholDto mainAlcoholDto)
+        {
+            if (!await _mainAlcoholService.IsMainAlcoholUnique(mainAlcoholDto.Name))
+            {
+                return BadRequest("Name is already used");
+            }
+
+            return Ok(await _mainAlcoholService.GetById(mainAlcoholDto.MainAlcoholId));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("delete/{id}")]
+        public async Task<ActionResult> Delete(int id, [FromBody] MainAlcoholDto mainAlcoholDto)
+        {
+            if (await _mainAlcoholService.IsMainAlcoholUsed(id))
+            {
+                return BadRequest("Main Alcohol is in use, cannot delete");
+            }
+
+
+            if (await _mainAlcoholService.Remove(id))
+                return Ok("Deleted");
+            else
+                return StatusCode(500);
         }
     }
 }
