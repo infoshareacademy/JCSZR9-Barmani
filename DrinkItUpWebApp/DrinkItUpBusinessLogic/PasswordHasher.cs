b440f2bbbaed49ddc80649fa754ff0e48ca8c384
kUcLis <ulis89@gmail.com>
2023-07-21 20:20:46 +0200

    Added User Service with Register Method and Password Hasher with Hashmethod and Verify method. Added PRofiles for maping

---
+++ DrinkItUpWebApp/DrinkItUpBusinessLogic/PasswordHasher.cs
@@ -0,0 +1,43 @@
+﻿using DrinkItUpBusinessLogic.Interfaces;
+using DrinkItUpWebApp.DAL.Repositories.Interfaces;
+
+
+namespace DrinkItUpBusinessLogic
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private readonly IUserRepository _userRepository;
+
+        public PasswordHasher(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public string Hash(string email, string password)
+        {
+            double length = password.Length + email.Length;
+
+            var hash = $"{email[0]}{Math.Pow(length, 3)}{password[4]}";
+
+            return hash;
+        }
+
+        public async Task<bool> Verify(string email, string password)
+        {
+            var user = await _userRepository.GetByEmail(email);
+            if (user == null)
+            {
+                return false;
+            }
+
+            var hash = Hash(email, password);
+            if (hash == user.PasswordHash)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
