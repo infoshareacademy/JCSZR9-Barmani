73ae2fd912ef53be3e3dd8567f0696bedfc92258
kUcLis <ulis89@gmail.com>
2023-07-21 20:04:03 +0200

    Added User Repostiry and interface for it

---
+++ DrinkItUpWebApp/DrinkItUpWebApp.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using DrinkItUpWebApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkItUpWebApp.DAL.Repositories
+{
+    public class UserRepository: CRUDRepository<User>, IUserRepository
+    {
+        private readonly DrinkContext _context;
+
+        public UserRepository(DrinkContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await _context.Users.FindAsync(email);
+        }
+    }
+}
