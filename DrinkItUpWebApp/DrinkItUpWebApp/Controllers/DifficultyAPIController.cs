bcd8ce3ac91e701f56b833c16b8f4f4de983ff0a
Łukasz Ostrowski <LukaszOst@gmail.com>
2023-07-23 12:44:45 +0200

    Added metod to Difficulty API Controller

--- DrinkItUpWebApp/DrinkItUpWebApp/Controllers/DifficultyAPIController.cs
+++ DrinkItUpWebApp/DrinkItUpWebApp/Controllers/DifficultyAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using DrinkItUpBusinessLogic;
+using DrinkItUpBusinessLogic.DTOs;
 using DrinkItUpBusinessLogic.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,59 @@ namespace DrinkItUpWebApp.Controllers
         {
             var difficultiesDtos = await _difficultyService.GetAll();
             return Ok(difficultiesDtos);
+        }
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var difficultyDto = await _difficultyService.GetById(id);
+            return Ok(difficultyDto);
+        }
+
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> Add([FromBody] DifficultyDto difficultyDto)
+        {
+            if (!await _difficultyService.IsDifficultyUnique(difficultyDto.Name))
+                return BadRequest("Name is already used");
+
+            var addedDififcultyDto = await _difficultyService.AddDifficulty(difficultyDto);
+            return Ok(addedDififcultyDto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("update")]
+
+        public async Task<ActionResult> Update([FromBody] MainAlcoholDto mainAlcoholDto)
+        {
+            if (!await _mainAlcoholService.IsMainAlcoholUnique(mainAlcoholDto.Name))
+            {
+                return BadRequest("Name is already used");
+            }
+
+            return Ok(await _mainAlcoholService.GetById(mainAlcoholDto.MainAlcoholId));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("delete/{id}")]
+        public async Task<ActionResult> Delete(int id, [FromBody] MainAlcoholDto mainAlcoholDto)
+        {
+            if (await _mainAlcoholService.IsMainAlcoholUsed(id))
+            {
+                return BadRequest("Main Alcohol is in use, cannot delete");
+            }
+
+
+            if (await _mainAlcoholService.Remove(id))
+                return Ok("Deleted");
+            else
+                return StatusCode(500);
         }
     }
 }
