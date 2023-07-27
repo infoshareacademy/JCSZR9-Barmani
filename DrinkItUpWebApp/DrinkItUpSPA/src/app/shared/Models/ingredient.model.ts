c97cc5a1846cc99b42e7e42361493211ba2d5733
kUcLis <ulis89@gmail.com>
2023-07-27 20:58:49 +0200

    Added Ingredient Panel Component

--- DrinkItUpWebApp/DrinkItUpSPA/src/app/shared/Models/ingredient.model.ts
+++ DrinkItUpWebApp/DrinkItUpSPA/src/app/shared/Models/ingredient.model.ts
@@ -2,9 +2,10 @@ import { UnitModel } from "./unit.model";
 
 export class IngredientModel{
     constructor(
-        public ingredientId: number,
-        public name: string,
         public unit: UnitModel,
+        public unitId?: number,
+        public ingredientId?: number,
+        public name?: string,
         public quantity?: number,
         public isUsed?: boolean
     ){}
